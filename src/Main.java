0f07fa1f855e645374ed313a775fa04a07281bd1
ArtemYakomazov <tema.yakomazov@gmail.com>
2025-07-22 12:51:49 +0300

    Выполнены условия домашки

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Author authorAnt = new Author("John", "Smith");
+        Author authorBeetle = new Author("Katy", "Parry");
+        Book ant = new Book("Ant", authorAnt, 2025);
+        Book beetle = new Book("Beetle", authorBeetle, 1997);
+        System.out.println("Название книги " + ant.getName());
+        System.out.println("Автор книги " + ant.getAuthor().getFirstName()+" "+ ant.getAuthor().getLastName());
+        System.out.println("Год публикации книги " + ant.getYearOfPublication());
+        ant.setYearOfPublication(2020);
+        System.out.println("Год публикации книги "+ ant.getYearOfPublication());
+        System.out.println("Название книги " + beetle.getName());
+        System.out.println("Автор книги " + beetle.getAuthor().getFirstName()+" "+ beetle.getAuthor().getLastName());
+        System.out.println("Год публикации книги " + beetle.getYearOfPublication());
     }
 }
