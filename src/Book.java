d687c0976388f264ffe1a4c302820e43486dc64d
ArtemYakomazov <tema.yakomazov@gmail.com>
2025-07-25 13:48:50 +0300

    Выполнены условия домашки: Методы объектов

--- src/Book.java
+++ src/Book.java
@@ -24,4 +24,21 @@ public class Book {
     public int getYearOfPublication() {
         return yearOfPublication;
     }
+
+    public String toString() {
+        return "Название книги: " + this.name + "; Автор книги: " + this.author + "; Год публикации книги: " + this.yearOfPublication;
+    }
+
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book book1 = (Book) other;
+        return name.equals(book1.name);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(name);
+    }
+
 }
